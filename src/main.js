f8c95011a28b6ca2e884a01227ad9ffd1360724c
Loshido <loshido@pm.me>
2022-05-08 19:58:47 +0200

    builded

--- src/main.js
+++ src/main.js
@@ -2,8 +2,10 @@ import { createApp } from "vue";
 import VueWriter from "vue-writer";
 import App from "./App.vue";
 import router from "./router/index.js"
+import { createPinia } from 'pinia'
 
-createApp(App)
-    .use(VueWriter)
-    .use(router)
-    .mount("#app");
+const app = createApp(App)
+app.use(VueWriter)
+app.use(router)
+app.use(createPinia())
+app.mount("#app");
---
+++ src/stores/index.js
@@ -0,0 +1,21 @@
+import {defineStore} from 'pinia'
+
+export const useStore = defineStore('main',{
+    
+})
+
+
+
+
+// @template
+///////////////////////////////////////////////////////
+// import { useStore } from '@/stores/counter'
+// export default {
+//   setup() {
+//     const store = useStore()
+//     return {
+//       store,
+//     }
+//   },
+// }
+///////////////////////////////////////////////////////
