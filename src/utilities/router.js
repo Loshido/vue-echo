96c8fe9896b115734c6d3993ef710bd9c6bdd07f
Loshido <loshido@pm.me>
2022-06-03 23:41:12 +0200

    Header position fixed

--- src/utilities/router.js
+++ src/utilities/router.js
@@ -3,7 +3,6 @@ import Home from "../components/Home.vue";
 import Page from "../components/Articles/Page.vue";
 import Article from "../components/Articles/Article.vue";
 import Life from "../components/Life/Life.vue";
-import auth from "../components/auth/auth.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -36,55 +35,16 @@ const router = createRouter({
       	component: Life,
     },
     {
-      	path: "/memo",
-      	name: "memo",
-      	component: () => import("../components/Life/Memo.vue"),
-    },
-    {
       	path: "/tools",
       	name: "tools",
-      	component: () => import("../components/tools/tools-handler.vue"),
+      	component: () => import("../views/tools.vue"),
+      	// component: () => import("../components/tools/tools-handler.vue"),
     },
 	{
-		path: "/tools/download-estimator",
-		name: "download",
-		component: () => import("../components/tools/download.vue")
-	},
-	{
-		path: "/tools/text-information",
-		name: "text-information",
-		component: () => import("../components/tools/text.vue")
-	},
-	{
-		path: "/tools/color",
-		name: "color",
-		component: () => import("../components/tools/color.vue")
-	},
-    {
-      	path: "/auth",
-      	name: "auth",
-      	component: auth,
-      	redirect: (to) => {
-      	  	return { path: "/auth/login" };
-      	},
-      	children: [
-      	  	{
-      	  	  	path: "login",
-				name: "login",
-      	  	  	component: () => import("../components/auth/login.vue"),
-      	  	},
-      	  	{
-      	  	  	path: "register",
-				name: "register",
-      	  	  	component: () => import("../components/auth/register.vue"),
-      	  	},
-      	  	{
-      	  	  	path: "logged",
-				name: "logged",
-      	  	  	component: () => import("../components/auth/logged.vue"),
-      	  	},
-      	],
-    },
+		path: "/tools/:tool",
+		name: "specific-tool",
+		
+	}
   ],
 });
 export default router;
